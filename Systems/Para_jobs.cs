1163bb7dfa35fce0f610b25644c4638f586149cd
chriscargill <chriscargill07@gmail.com>
2021-02-24 22:13:16 -0500

    Completed a Parallel Job Changes to be committed: 	modified:   Systems/Para_jobs.cs

--- Systems/Para_jobs.cs
+++ Systems/Para_jobs.cs
@@ -5,42 +5,57 @@ using Unity.Jobs;
 public class Para_jobs : MonoBehaviour
 {
     // Job adding two floating point values together
-    public struct MyJob : IJob
+    public struct OurJobs : IJobParallelFor
     {
         [ReadOnly]
-        public float a;
+        public NativeArray<float> a;
         [ReadOnly]
-        public float b;
+        public NativeArray<float> b;
         public NativeArray<float> result;
 
-        public void Execute()
+        public void Execute(int i)
         {
-            result[0] = a + b;
+            result[i] = a[i] + b[i];
         }
     }
     public void Update()
     {
         // Create a native array of a single float to store the result. This example waits for the job to complete for illustration purposes
-        NativeArray<float> result = new NativeArray<float>(1, Allocator.TempJob);
+        NativeArray<float> a = new NativeArray<float>(3, Allocator.TempJob);
+        NativeArray<float> b = new NativeArray<float>(3, Allocator.TempJob);
+        NativeArray<float> result = new NativeArray<float>(3, Allocator.TempJob);
 
         // Set up the job data
-        MyJob jobData = new MyJob();
-        jobData.a = 5;
-        jobData.b = 10;
+        a[0] = 1337;
+        b[0] = 2021;
+        a[1] = 10;
+        b[1] = 20;
+        a[2] = 2;
+        b[2] = 4;
+
+        OurJobs jobData = new OurJobs();
+        jobData.a = a;
+        jobData.b = b;
         jobData.result = result;
 
         // Schedule the job
-        JobHandle handle = jobData.Schedule();
+        JobHandle handle = jobData.Schedule(3, 32);
 
         // Wait for the job to complete
         handle.Complete();
 
         // All copies of the NativeArray point to the same memory, you can access the result in "your" copy of the NativeArray
         float aPlusB = result[0];
+        float two = result[1];
+        float three = result[2];
 
         // Free the memory allocated by the result array
+        a.Dispose();
+        b.Dispose();
         result.Dispose();
 
         Debug.Log(aPlusB);
+        Debug.Log(two);
+        Debug.Log(three);
     }
 }
